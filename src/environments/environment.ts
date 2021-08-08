39b49e717510f396cb697301fe28ba93b2cb8c1e
Tobin Smit <tobinksmit@gmail.com>
2021-08-08 12:15:51 +1000

    Configuring project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAeK2gJjvy6OLda2ovgiULG8v3a5GIx53s",
+    authDomain: "feedback-network-ts.firebaseapp.com",
+    projectId: "feedback-network-ts",
+    storageBucket: "feedback-network-ts.appspot.com",
+    messagingSenderId: "628778369858",
+    appId: "1:628778369858:web:929a9498e79712f8fd3edb",
+    measurementId: "G-KHZT2MFNR8"
+  }
 };
 
 /*
